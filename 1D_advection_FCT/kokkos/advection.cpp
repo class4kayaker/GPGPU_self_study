c4ff197d6f2f5916449e5011999484328dcad0b3
Jonathan Robey <class4kayaker@gmail.com>
2020-03-31 10:51:31 -0700

    Get to semi-functional state

--- 1D_advection_FCT/kokkos/advection.cpp
+++ 1D_advection_FCT/kokkos/advection.cpp
@@ -12,12 +12,14 @@ int main(int argc, char *argv[]) {
   const double sigma = 0.9;
   const double a = 3.0;
 
-  FCT_initialization::ProblemConfig config = FCT_initialization::parse_args(argc, argv);
+  FCT_initialization::ProblemConfig config =
+      FCT_initialization::parse_args(argc, argv);
 
   const int ndx = config.ndx;
 
   // Check sizes.
-  
+  FCT_initialization::check_bounds(config);
+
   struct FCT_initialization::InitState init(ndx);
 
   FCT_initialization::sine_init(init, config, config.init_time);
@@ -27,85 +29,86 @@ int main(int argc, char *argv[]) {
   Kokkos::initialize(argc, argv);
   {
     // Allocate y, x vectors and Matrix A:
-    double *const u_state = new double[ndx+2*2];
-    double *const flux_low = new double[ndx+1];
-    double *const flux_high = new double[ndx+1];
-    double *const flux_c = new double[ndx+1];
+    double *const u_state = new double[ndx + 2 * 2];
+    double *const flux_low = new double[ndx + 1];
+    double *const flux_high = new double[ndx + 1];
+    double *const adiff_flux = new double[ndx + 1];
+    double *const flux_c = new double[ndx + 1];
 
     // Initialize U
-    for (int i=0; i<ndx; ++i){
-        u_state[i+2] = init.u[i];
+    for (int i = 0; i < ndx; ++i) {
+      u_state[i + 2] = init.u[i];
     }
-
-    // State update loop
-    for (int timestep=0; timestep<config.ndt; ++timestep)
-    {
-        // Compute low order flux
-
-        // Compute high order flux
-
-        // Compute FCT flux
-        
-        // Do update
-    }
-
-    // Initialize y vector.
-    Kokkos::parallel_for("y_init", N, KOKKOS_LAMBDA(int i) { y[i] = 1; });
-
-    // Initialize x vector.
-    Kokkos::parallel_for("x_init", M, KOKKOS_LAMBDA(int i) { x[i] = 1; });
-
-    // Initialize A matrix, note 2D indexing computation.
-    Kokkos::parallel_for("matrix_init", N, KOKKOS_LAMBDA(int j) {
-      for (int i = 0; i < M; ++i) {
-        A[j * M + i] = 1;
-      }
-    });
 
     // Timer products.
     Kokkos::Timer timer;
 
-    for (int repeat = 0; repeat < nrepeat; repeat++) {
-      // Application: <y,Ax> = y^T*A*x
-      double result = 0;
+    // State update loop
+    for (int timestep = 0; timestep < config.ndt; ++timestep) {
+      const double dtdx = (config.dt / init.dx);
+      const double dxdt = (init.dx / config.dt);
 
-      Kokkos::parallel_reduce("yAx", N,
-                              KOKKOS_LAMBDA(int j, double &update) {
-                                double temp2 = 0;
+      // Set BCs
+      u_state[0] = u_state[ndx + 1];
+      u_state[1] = u_state[ndx + 2];
+      u_state[ndx + 3] = u_state[2];
+      u_state[ndx + 4] = u_state[3];
 
-                                for (int i = 0; i < M; ++i) {
-                                  temp2 += A[j * M + i] * x[i];
-                                }
+      // Compute fluxes
+      Kokkos::parallel_for("calc_low_flux", ndx + 1, KOKKOS_LAMBDA(int i) {
+        flux_low[i] = config.a * u_state[i + 1];
+      });
 
-                                update += y[j] * temp2;
-                              },
-                              result);
+      // Compute high order flux
+      const double sigma_i = (config.a * config.dt / init.dx);
+      Kokkos::parallel_for("calc_high_flux", ndx + 1, KOKKOS_LAMBDA(int i) {
+        flux_high[i] =
+            config.a * 0.5 *
+            ((1 + sigma_i) * u_state[i + 1] + (1 - sigma_i) * u_state[i + 2]);
+      });
 
-      // Output result.
-      const double solution = (double)N * (double)M;
+      // Compute diff flux
+      Kokkos::parallel_for("calc_diff_flux", ndx + 1, KOKKOS_LAMBDA(int i) {
+        adiff_flux[i] = flux_high[i] - flux_low[i];
+      });
 
-      if (result != solution) {
-        printf("  Error: result( %lf ) != solution( %lf )\n", result, solution);
-      }
+      // Do update with low flux
+      Kokkos::parallel_for("low_flux_update", ndx, KOKKOS_LAMBDA(int i) {
+        u_state[i + 2] += dtdx * (flux_low[i] - flux_low[i + 1]);
+      });
+
+      // Set BCs
+      u_state[0] = u_state[ndx + 1];
+      u_state[1] = u_state[ndx + 2];
+      u_state[ndx + 3] = u_state[2];
+      u_state[ndx + 4] = u_state[3];
+
+      // Compute FCT flux
+      Kokkos::parallel_for("calc_fct_flux", ndx + 1, KOKKOS_LAMBDA(int i) {
+        double sign_a = copysign(1.0, adiff_flux[i]);
+        double left_d = u_state[i + 1] - u_state[i + 0];
+        double right_d = u_state[i + 3] - u_state[i + 2];
+        flux_c[i] =
+            sign_a * std::max(0.0, std::min(std::min(sign_a * dxdt * left_d,
+                                                     sign_a * dxdt * right_d),
+                                            abs(adiff_flux[i])));
+      });
+
+      // Do update
+      Kokkos::parallel_for("full_update", ndx, KOKKOS_LAMBDA(int i) {
+        u_state[i + 2] += dtdx * (flux_c[i] - flux_c[i + 1]);
+      });
     }
-
     double time = timer.seconds();
 
-    // Calculate bandwidth.
-    // Each matrix A row (each of length M) is read once.
-    // The x vector (of length M) is read N times.
-    // The y vector (of length N) is read once.
-    // double Gbytes = 1.0e-9 * double( sizeof(double) * ( 2 * M * N + N ) );
-    double Gbytes = 1.0e-9 * double(sizeof(double) * (M + M * N + N));
-
     // Print results (problem size, time and bandwidth in GB/s).
-    printf("  N( %d ) M( %d ) nrepeat ( %d ) problem( %g MB ) time( %g s ) "
-           "bandwidth( %g GB/s )\n",
-           N, M, nrepeat, Gbytes * 1000, time, Gbytes * nrepeat / time);
+    printf("Time for %d timestep computation %g s\n", config.ndt, time);
 
-    delete[] A;
-    delete[] y;
-    delete[] x;
+    delete[] u_state;
+    delete[] flux_low;
+    delete[] flux_high;
+    delete[] adiff_flux;
+    delete[] flux_c;
   }
   Kokkos::finalize();
 
--- 1D_advection_FCT/utils/advection_utils.h
+++ 1D_advection_FCT/utils/advection_utils.h
@@ -9,9 +9,10 @@ namespace FCT_initialization {
 const double PI = 3.141592653589793;
 
 struct ProblemConfig {
-  ProblemConfig(const int ndx, const double a, const double sigma, const double init_time, const double end_time);
+  ProblemConfig(const int ndx, const double a, const double sigma,
+                const double init_time, const double end_time);
 
-  compute_timestep(const double dx);
+  void compute_timestep(const double dx);
 
   const int ndx;
   const double a;
@@ -20,17 +21,17 @@ struct ProblemConfig {
   const double end_time;
 
   // Derived values
-  double ndt;
+  int ndt;
   double dt;
-}
+};
 
-ProblemConfig::ProblemConfig(const int ndx, const double a, const double sigma, const double init_time, const double end_time)
-    : ndx(ndx), a(a), sigma(sigma), init_time(init_time), end_time(end_time) {
-}
+ProblemConfig::ProblemConfig(const int ndx, const double a, const double sigma,
+                             const double init_time, const double end_time)
+    : ndx(ndx), a(a), sigma(sigma), init_time(init_time), end_time(end_time) {}
 
-ProblemConfig::compute_timestep(const double dx){
-    ndt = std::ceil(a*(end_time-init_time)/(sigma*dx));
-    dt = (end_time-init_time)/ndt;
+void ProblemConfig::compute_timestep(const double dx) {
+  ndt = std::ceil(a * (end_time - init_time) / (sigma * dx));
+  dt = (end_time - init_time) / ndt;
 }
 
 struct InitState {
@@ -41,18 +42,16 @@ struct InitState {
   double time;
   std::vector<double> px;
   std::vector<double> u;
-}
+};
 
-InitState::InitState(const int ndx)
-    : ndx(ndx), time(0.0), px(ndx), u(ndx) {
-}
+InitState::InitState(const int ndx) : ndx(ndx), time(0.0), px(ndx, 0.0), u(ndx, 0.0) {}
 
 void sine_init(InitState &state, const ProblemConfig &config,
                const double time) {
   state.time = time;
-  state.dx = 1.0/ndx;
+  state.dx = 1.0 / state.ndx;
 
-  for (const int i = 0; i < state.ndx; ++i) {
+  for (int i = 0; i < state.ndx; ++i) {
     state.px[i] = (i + 0.5) * state.dx;
     state.u[i] = sin((state.px[i] - time * config.a) * 2 * PI);
   }
@@ -62,6 +61,7 @@ struct ProblemConfig parse_args(int argc, char *argv[]) {
   int ndx = -1;
   const double a = 3.0;
   double sigma = 0.9;
+  double end_time = 0.0;
 
   // Read command line arguments.
   for (int i = 0; i < argc; i++) {
@@ -70,7 +70,7 @@ struct ProblemConfig parse_args(int argc, char *argv[]) {
       printf("  User ndx is %d\n", ndx);
     } else if ((strcmp(argv[i], "-T") == 0)) {
       end_time = atof(argv[++i]);
-      printf("  User end_time is %d\n", M);
+      printf("  User end_time is %g\n", end_time);
     } else if ((strcmp(argv[i], "-h") == 0) ||
                (strcmp(argv[i], "-help") == 0)) {
       printf("  -help (-h):            print this message\n\n");
@@ -78,8 +78,15 @@ struct ProblemConfig parse_args(int argc, char *argv[]) {
     }
   }
 
-  struct ProblemConfig to_return(ndx, a, sigma);
+  struct ProblemConfig to_return(ndx, a, sigma, 0.0, end_time);
 
   return to_return;
+}
+
+void check_bounds(const ProblemConfig &config) {
+    if(config.ndx <=0){
+        printf("NDX <= 0, cannot run computation\n");
+        exit(1);
+    }
 }
 } // namespace FCT_initialization
