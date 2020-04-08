54d3012c8816919c7cc5bae8befac179d367e359
Jonathan Robey <class4kayaker@gmail.com>
2020-04-08 09:57:33 -0700

    change to relative imports in __init__.py

--- 1D_advection_FCT/python_validation/src/fdm_validation_utility/__init__.py
+++ 1D_advection_FCT/python_validation/src/fdm_validation_utility/__init__.py
@@ -3,14 +3,14 @@ and checking the error using HDF5 files generated"""
 
 __version__ = "0.0.1"
 
-from fdm_validation_utility.problem_state import (
+from .problem_state import (
     FDM_Problem_Config,
     FDM_Advection_State,
 )
 
-from fdm_validation_utility.initial_conditions import init_by_name
+from .initial_conditions import init_by_name
 
-from fdm_validation_utility.calculate_error import FDM_Error
+from .calculate_error import FDM_Error
 
 
 __all__ = [
